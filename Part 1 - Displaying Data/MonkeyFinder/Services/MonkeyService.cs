34232692260142fab0be1720a01d4b98a3423954
Carlos Gomez <67838217+CarlosNetAcad@users.noreply.github.com>
2024-01-18 21:24:27 -0500

    [HTPP REQUEST][HttpClient]

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,12 +1,32 @@
-﻿using MonkeyFinder.Interfaces.Services;
+﻿using System.Net.Http.Json;
+using MonkeyFinder.Interfaces.Services;
 
 namespace MonkeyFinder.Services;
 
 public class MonkeyService : IMonkeyService
 {
+    HttpClient httpClient;
+
+    IList<Monkey> monkeyList = new List<Monkey>();
+
+    public MonkeyService()
+    {
+        httpClient = new();
+    }
+
     public async Task<IList<Monkey>> GetMonkeys()
     {
-        IList<Monkey> monkeyList = new List<Monkey>();
+        if( monkeyList?.Count > 0)
+            return monkeyList;
+
+        string url = "https://montemagno.com/monkeys.json";
+
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
 
         return monkeyList;
     }
