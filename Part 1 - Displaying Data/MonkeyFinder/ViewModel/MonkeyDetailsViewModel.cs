befdeb45801db8537b0dc1d62dbf68cf0a38c05d
Carlos Gomez <67838217+CarlosNetAcad@users.noreply.github.com>
2024-01-19 23:11:38 -0500

    [NAVIGATION] Shell

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,8 +1,13 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
+[QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
+    [ObservableProperty]
+    Monkey monkey;
+
     public MonkeyDetailsViewModel(string title) : base(title)
     {
+        
     }
 }
